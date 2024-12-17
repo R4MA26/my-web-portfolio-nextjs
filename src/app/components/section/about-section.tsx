d77f1d72afdd4dbea3d86459a7ea807675d78407
Rama Ismaya Sofyan <ramaismaya25@gmail.com>
2024-12-17 14:14:09 +0000

    fix: responsive about section

--- src/app/components/section/about-section.tsx
+++ src/app/components/section/about-section.tsx
@@ -6,16 +6,15 @@ export function AboutSection() {
       ' '
     );
   return (
-    <div className='flex h-fit items-center'>
+    <div className='flex h-fit flex-col items-center md:flex-row'>
       <div className='flex flex-1 justify-center'>
         <img
-          src={'images/rama.jpg'}
-          alt={'alt'}
-          className='rounded-full border-2 border-gray-300 object-cover dark:border-none'
-          style={{ width: 400, height: 400 }}
+          src='images/rama.jpg'
+          alt='alt'
+          className='h-72 w-72 rounded-full border-2 border-gray-300 object-cover dark:border-none md:h-96 md:w-96'
         />
       </div>
-      <div className='flex-1 text-3xl leading-loose tracking-widest'>
+      <div className='flex-1 text-2xl tracking-widest md:text-3xl md:leading-loose'>
         {text.map((el, i) => (
           <motion.span
             initial={{ opacity: 0 }}
